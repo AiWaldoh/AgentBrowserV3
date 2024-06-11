b1d8940a77aa16346342327abe35ee12a461882e
AiWaldoh <kevin@gptcoder.pro>
2024-06-11 15:46:42 -0300

    added types to main file

--- src/main.py
+++ src/main.py
@@ -10,42 +10,46 @@ nltk.download("punkt")
 
 class ChatbotApp:
     def __init__(self):
-        self.api_key = Config.OPENAI_API_KEY
-        self.system_message = Config.SYSTEM_MESSAGE
-        self.tools_loader = self.setup_tools_loader()
+        self.api_key: str = Config.OPENAI_API_KEY
+        self.system_message: str = Config.SYSTEM_MESSAGE
+        self.chat_api_service: ChatAPIService
+        self.response_handler: ResponseHandler
+        self.tools_loader: ToolsLoader = self.setup_tools_loader()
         self.chat_api_service, self.response_handler = self.setup_chat_services()
 
-    def get_absolute_path(self, relative_path):
+    def get_absolute_path(self, relative_path) -> str:
         base_dir = os.path.dirname(os.path.abspath(__file__))
         return os.path.join(base_dir, "..", "config", "tools.yaml")
 
-    def setup_tools_loader(self):
+    def setup_tools_loader(self) -> str:
         tools_definition_file = self.get_absolute_path("../config/tools.yaml")
         tools_loader = ToolsLoader(tools_definition_file)
         tools_loader.load_tools()
         return tools_loader
 
-    def setup_chat_services(self, model_name="gpt-4o", temperature=1.0):
+    def setup_chat_services(
+        self, model_name="gpt-4o", temperature=1.0
+    ) -> tuple[ChatAPIService, ResponseHandler]:
         chat_api_service = ChatAPIService(self.api_key, model_name, temperature)
         response_handler = ResponseHandler()
         return chat_api_service, response_handler
 
-    def initialize_new_message(self):
+    def initialize_new_message(self) -> list[dict[str, str]]:
         return [{"role": "system", "content": self.system_message}]
 
-    def is_quit_command(self, user_input):
+    def is_quit_command(self, user_input) -> bool:
         return user_input.lower() in ["quit", "exit"]
 
-    def add_user_message(self, messages, user_input):
+    def add_user_message(self, messages, user_input) -> None:
         messages.append({"role": "user", "content": user_input})
 
-    def add_assistant_message(self, messages, assistant_response):
+    def add_assistant_message(self, messages, assistant_response) -> None:
         messages.append({"role": "assistant", "content": assistant_response})
 
-    def extract_assistant_response(self, response):
+    def extract_assistant_response(self, response) -> str:
         return response["choices"][0]["message"]["content"]
 
-    async def run(self):
+    async def run(self) -> None:
         while True:
             messages = self.initialize_new_message()
             user_input = input("User: ")
@@ -62,7 +66,7 @@ class ChatbotApp:
             # self.add_assistant_message(messages, assistant_response)
 
 
-async def main():
+async def main() -> None:
     app = ChatbotApp()
     await app.run()
 
